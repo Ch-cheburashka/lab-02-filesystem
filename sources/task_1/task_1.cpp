aed3bbac337f32d0c5b930e496d2e5acbdbf39f5
Ch-cheburashka <m9056883999@gmail.com>
2023-03-06 12:41:45 +0300

    used the concept oop concept for task_1

--- sources/task_1/task_1.cpp
+++ sources/task_1/task_1.cpp
@@ -1,6 +1,20 @@
 #include <task_1/task_1.hpp>
-#include <custom_iterator/custom_iterator.hpp>
-#include <constants.hpp>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <broker/broker.hpp>
+
+std::vector<std::string> components_extractor(const path &p) {
+    std::vector<std::string> components;
+    std::string s = p.string();
+    for (auto it = s.find('_'); it != std::string::npos; s = s.substr(it + 1), it = s.find('_')) {
+        components.push_back(s.substr(0, it));
+    }
+    components.push_back(s.substr(0, s.find('.')));
+    if (components.size() > 1)
+        components.push_back(s.substr(s.find('.'), s.length()));
+    return components;
+}
 
 std::string broker_seeker(const path &p) {
     std::string res, cur = p.string();
@@ -11,64 +25,69 @@ std::string broker_seeker(const path &p) {
 }
 
 bool filename_analyzer(const std::string &s) {
-    std::vector<std::string> components = custom_iterator::components_extractor(s);
+    std::vector<std::string> components = components_extractor(s);
     if (components[1].length() == 8 && components[2].length() == 8 && components[3] == ".txt" &&
-        components[0].erase(0,s.find(broker_seeker(components[0])) + broker_seeker(components[0]).length() +1) ==
+        components[0].erase(0, s.find(broker_seeker(components[0])) + broker_seeker(components[0]).length() + 1) ==
         "balance")
         return true;
     return false;
 }
 
-void report_printer(custom_iterator &&c, const std::string &br) {
-    for (auto &i: c.get_broker()) {
-        std::cout << "broker:" << br << " account:" << i.first << " files:" << i.second.size() << " lastdate: "
-                  << i.second[i.second.size() - 1] << "\n";
-    }
-}
-
-void iterating(const path &p, bool print) {
-    std::string list;
-    std::string res, ib_s, bcs_s, otk_s;
+void iteration(const path &p, bool files_output) {
+    std::vector<broker> brokers;
     for (auto &entry: std::filesystem::directory_iterator{p}) {
         if (entry.is_directory()) {
-            if (print)
-                iterating(entry.path(), true);
-            else
-                iterating(entry.path(), false);
+            iteration(entry.path(),files_output);
         }
         if (entry.is_directory() || !filename_analyzer(entry.path().string())) {
             continue;
         }
-        if (print)
+        if (files_output)
             std::cout << broker_seeker(entry.path().string()) << " "
                       << entry.path().string().erase(0, entry.path().string().find("balance")) << std::endl;
         else {
-            list += broker_seeker(entry.path().string()) + " " +
-                    entry.path().string().erase(0, entry.path().string().find("balance")) + '\n';
+            std::string br = broker_seeker(entry.path());
+            auto components = components_extractor(entry.path());
+            if (brokers.empty() || std::find_if(brokers.begin(), brokers.end(),
+                                                [&br](const broker &broker) { return broker._name == br; }) ==
+                                   brokers.end()) {
+                std::list<path> files;
+                files.push_back(entry.path());
+                account ac(components[1], files);
+                std::vector<account> accounts;
+                accounts.emplace_back(ac);
+                brokers.emplace_back(br, accounts);
+            } else {
+                size_t broker_index = 0;
+                for (; broker_index < brokers.size(); ++broker_index) {
+                    if (brokers[broker_index]._name == broker_seeker(entry.path()))
+                        break;
+                }
+                auto accounts = brokers[broker_index]._accounts;
+                std::string ac = components[1];
+                if (accounts.empty() || std::find_if(accounts.begin(), accounts.end(),
+                                                     [&ac](const account &account) { return account._name == ac; }) == accounts.end()) {
+                    std::list<path> files;
+                    files.push_back(entry.path());
+                    brokers[broker_index]._accounts.emplace_back(ac, files);
+                } else {
+                    for (size_t account_index = 0; account_index < accounts.size(); ++account_index) {
+                        if (accounts[account_index]._name == components[1]) {
+                            brokers[broker_index]._accounts[account_index]._files.push_back(entry.path());
+                            break;
+                        }
+                    }
+                }
+            }
         }
     }
-    if (!print) {
-        for (size_t i = 0; i < list.length(); ++i) {
-            if (list.substr(i, BCS_BR_INDENT) == BCS_STR) {
-                bcs_s.append(list.substr(i, BCS_LINE_INDENT));
-            }
-            if (list.substr(i, IB_BR_INDENT) == IB_STR) {
-                ib_s.append(list.substr(i, IB_LINE_INDENT));
-            }
-            if (list.substr(i, OTK_BR_INDENT) == OTK_STR) {
-                otk_s.append(list.substr(i, OTK_LINE_INDENT));
+    if (!files_output) {
+        for (auto &broker: brokers) {
+            for (auto &account : broker._accounts) {
+                auto components = components_extractor(account._files.back());
+                std::cout << "broker:" << broker._name << " account:" << account._name << " files:"
+                          << account._files.size() << " lastdate:" << components[2] << "\n";
             }
         }
-        std::map<std::string, std::vector<std::string>> map_bcs;
-        custom_iterator ci_bcs(map_bcs, BCS_LINE_INDENT, bcs_s);
-        report_printer(ci_bcs.info_gatherer(), BCS_STR);
-
-        std::map<std::string, std::vector<std::string>> map_ib;
-        custom_iterator ci_ib(map_ib, IB_LINE_INDENT, ib_s);
-        report_printer(ci_ib.info_gatherer(), IB_STR);
-
-        std::map<std::string, std::vector<std::string>> map_otk;
-        custom_iterator ci_otk(map_otk, OTK_LINE_INDENT, otk_s);
-        report_printer(ci_otk.info_gatherer(), OTK_STR);
     }
 }
